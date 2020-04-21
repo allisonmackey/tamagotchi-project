c0e001b45c5346c9c32fe722c907bfac2243b25b
Allison Mackey <a.mackey693@gmail.com>
2020-04-21 16:22:43 -0700

    add additional API calls for all three animal options and use css / bootstrap to position them on the page.

--- src/main.js
+++ src/main.js
@@ -3,31 +3,72 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-// import { Animal } from './../src/tamagotchi.js'
-// ("<p>Your current age on Mercury is: "+ newAge.all[0] +"</p>");
+// import { Animal } from './../src/tamagotchi.js';
+
 $(document).ready(function() {
+    //API NIGHT FURY 
+  let request = new XMLHttpRequest();
+  let url = `https://api.giphy.com/v1/gifs/search?q=happy-cute-dragon-sticker&api_key=${process.env.GIPHY_KEY}&limit=25&offset=12`
 
-  $('#giphyButton').click(function() {
+ 
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  };
 
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?q=dragon&api_key=${process.env.GIPHY_KEY}&limit=1&rating=G&lang=en`;
+  request.open("GET", url, true);
+  request.send();
 
+
+  let getElements = function(response) {
+    $('.night-fury').append("<img class='icon' src='" +`${response.data[0].images.downsized_large.url}` + "' alt='night-fury'>");
+  };
     
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
+  // LIGHT FURY API 
+  let request2 = new XMLHttpRequest();
+  let url2 = `https://api.giphy.com/v1/gifs/search?q=light-fury-sticker&api_key=${process.env.GIPHY_KEY}&limit=3&offset=2&rating=G&lang=en`
 
-    request.open("GET", url, true);
-    request.send();
-
-    const getElements = function(response) {
-      $('.showGif').append("<img src='" +`${response.data[0].images.downsized_large.url}` + "'>")
-      console.log(response);
-      // $('.showGif').html("<img src = '${response.data[0].url}'  + "alt='dragon'>"");
-      // data[""0""].images.downsized_large.url
+  request2.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response2 = JSON.parse(this.responseText);
+      getElements2(response2);
     }
-  });
+  };
+
+  request2.open("GET", url2, true);
+  request2.send();
+
+  let getElements2 = function(response2) {
+    $('.light-fury').append("<img class='icon' src='" +`${response2.data[0].images.downsized_large.url}` + "' alt='light-fury'>");
+  };
+
+
+  // API OTHER FURY 
+  let request3 = new XMLHttpRequest
+  let url3 = `https://api.giphy.com/v1/gifs/search?q=happy-cute-dragon-sticker&api_key=${process.env.GIPHY_KEY}&limit=25&offset=9`
+
+  request3.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response3 = JSON.parse(this.responseText);
+      getElements3(response3);
+    }
+  };
+
+  request3.open("GET", url3, true);
+  request3.send();
+
+  let getElements3 = function(response3) {
+    $('.other-fury').append("<img class='icon' src='" +`${response3.data[0].images.downsized_large.url}` + "' alt='other-fury'>");
+  };
+
+  console.log("response2 :", response2)
+  console.log("response :", response)
+  
 });
+
+  // // $('#night-fury').click(function() {
+  //   const nightFury = new Animal(10, 10, 10);
+  //   nightFury.timedDecrease();
+  //   // });
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -8,23 +8,23 @@ export class Animal {
   
   statusCheck() {
     if (this.hunger <= 0) {
-      this.hungerStatus = "Your animal is dead!!"
+      this.hungerStatus = "Your animal is dead!!";
     }else if (this.hunger <= 3) {
-      this.hungerStatus = "Your animal is hungry!!"
+      this.hungerStatus = "Your animal is hungry!!";
     }
 
     if (this.rest <= 0) {
-      this.restStatus = "Your animal is dead!!"
+      this.restStatus = "Your animal is dead!!";
     } else if (this.rest <= 3) {
-      this.restStatus = "Your animal is tired!!"
+      this.restStatus = "Your animal is tired!!";
     } 
 
     if (this.attention <= 0) {
-      this.attentionStatus = "Your animal is dead!!"
+      this.attentionStatus = "Your animal is dead!!";
     } else if (this.attention <= 3) {
-      this.attentionStatus = "Your animal is needy!!"
+      this.attentionStatus = "Your animal is needy!!";
     } 
-  };
+  }
 
   timedDecrease() {
     setInterval(() => {
@@ -35,39 +35,27 @@ export class Animal {
     }, 30000);
     setInterval(() => {
       this.attention--;
-      this.statusCheck()
+      this.statusCheck();
     }, 10000);
 
   }
 
   feed() {
     if (this.hunger < 21) {
-      this.hunger++
+      this.hunger++;
     }
   }
 
   sleep() {
     if (this.rest < 21) {
-      this.rest++
+      this.rest++;
     }
   }
 
   love() {
     if (this.attention < 21) {
-      this.attention++
+      this.attention++;
     }
   }
 
 }
-
-// Attention decreases every 10 seconds (10,000) 
-  // MAX level = 21 "you've reached the max"
-  // LOW level = 3? "tamagotchi barely alive"
-  // DEATH level = 0 "tamagotchi ded"
-// Hunger decreases every 20 seconds (20,000) 
-  // MAX level = 21 & increases by 1 every click
-  // LOW level = 3 "tamagotchi is barely alive"
-  // DEATH level = 0 "tamagotchi ded now"
-// Rest decreases every 30 seconds (30,000)
-  // MAX rest level = 21 
-  // LOW level =  "tamagotchi is barely alive!"
